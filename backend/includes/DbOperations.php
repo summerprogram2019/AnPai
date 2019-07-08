26bfb74d67eba3fafa35d3532e6eee2641190f19
Frank  Mudole <frankmudole@Franks-MacBook-Pro.local>
2019-07-08 10:32:22 +0800

    Update: Functionality that checks whether user exist.

--- backend/includes/DbOperations.php
+++ backend/includes/DbOperations.php
@@ -18,44 +18,88 @@ class DbOperations{
 		$db = new DbConnect();
 
 		$this->con = $db->connect();
-	}
-/* CRUD --> Create*/
+	};
+/* Create.Read.Update.Delete (CRUD) --> Create*/
 
-function createUser($username, $email, $password){
+	public function createUser($username, $email, $pass){
 
-	/*function createUser creates the user data base
-	 Parameters:
-				 $username (VARCHAR): 100 characters
-				 $email (VARCHAR): 100 characters
-				 $password (Text): N/A
-	 Variables:
-				$stmt: Prepares the function that will use to send the input into the
-							data base. Where the in built function prepare contains sql.
-	*/
+		/*function createUser creates the user data base
+		 Parameters:
+					 $username (VARCHAR): 100 characters
+					 $email (VARCHAR): 100 characters
+					 $password (Text): N/A
+		 Variables:
+					$stmt: Prepares the function that will use to send the input into the
+								data base. Where the in built function prepare contains sql.
+		*/
 
-	$password = md5($pass);
+		if ($this->isUserExist($username,$email)) {
+			// the code checks whether the user already exist in the database.
 
-	 //prepare function will have an sql insert query
-	$stmt = $this->con->prepare("INSERT INTO `user` (`id`, `username`, `password`,
-		 `email`) VALUES (NULL, ?,?,?);");
-		// this binds the parameters needed for the sql querey
-	$stmt->bind_param("sss",$username, $password, $email);
-
-	// This line will be called and data will be inseted into our database.
-	//stmt->execute
-	if($stmt->execute()) {
-
-			return true;
+			return 0;
 
 		} else {
 
-			return false;
+			$password = md5($pass);
+			 //prepare function will have an sql insert query
+			 // things that must be done, is to construct checkstatement that ensures
+			 //username and email has to be unquine.
+			$stmt = $this->con->prepare("INSERT INTO `user` (`id`, `username`, `password`,
+				 `email`) VALUES (NULL, ?,?,?);");
+				// this binds the parameters needed for the sql querey
+			$stmt->bind_param("sss",$username, $password, $email);
 
-				}
+			// This line will be called and data will be inseted into our database.
+			//stmt->execute
+			if($stmt->execute()) {
+
+				return 1; // user does not exist in the database. return 1.
+
+			} else {
+
+				return 2; // error occurs return 2.
+
+			}
 
 		}
 
+	}
 
+
+	private  function isUserExist ($username,$email) {
+		/*
+
+		This function will check whether a user already exist and will controll
+		the interaction accordingly.
+
+		Parameters:
+
+						$username(VARCHAR):
+						$pass(text):
+						$email():
+
+
+		Variable:
+
+						stmt (num): returns true or false. In built function with SQL
+												code.
+
+		*/
+
+		$stmt = $this->con->prepare("SELECT id FROM users WHERE username = ?
+			OR email = ? ");
+		//if value is return from the above query then user already exist in
+		//database.
+		$stmt -> bind_param("ss", $username, $email);
+
+		$stmt = store_result();
+
+		return $stmt->num_rows > 0; //num_rows gets the value from and determines
+																//whether user exists. If num_rows is above 0
+																// then user already exists.
+
+
+	}
 
 }
  ?>
